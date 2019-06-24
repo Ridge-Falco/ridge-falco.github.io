4b6453ae0a43ed70d37d2fbf31f3980f63f7d56b
Ridge Falco <holyswigpig48.games@gmail.com>
2019-06-24 15:02:01 -0400

    Dones Home Page

---
+++ index.js
@@ -0,0 +1,52 @@
+function widthHundredHome() {
+    document.getElementById('home').style.width = '100%';
+    document.getElementById('langs').style.display = 'none';
+    document.getElementById('acts').style.display = 'none';
+    document.getElementById('hobbies').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+}
+
+function widthHundredLangs() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('langs').style.width = '100%';
+    document.getElementById('acts').style.display = 'none';
+    document.getElementById('hobbies').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+}
+
+function widthHundredActs() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('langs').style.display = 'none';
+    document.getElementById('acts').style.width = '100%';
+    document.getElementById('hobbies').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+}
+
+function widthHundredHobbies() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('langs').style.display = 'none';
+    document.getElementById('acts').style.display = 'none';
+    document.getElementById('hobbies').style.width = '100%';
+    document.getElementById('projects').style.display = 'none';
+}
+
+function widthHundredProjects() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('langs').style.display = 'none';
+    document.getElementById('acts').style.display = 'none';
+    document.getElementById('hobbies').style.display = 'none';
+    document.getElementById('projects').style.width = '100%';
+}
+
+function widthNormal() {
+    document.getElementById('home').style.width = '20%';
+    document.getElementById('langs').style.width = '20%';
+    document.getElementById('acts').style.width = '20%';
+    document.getElementById('hobbies').style.width = '20%';
+    document.getElementById('projects').style.width = '20%';
+    document.getElementById('home').style.display = 'inline';
+    document.getElementById('langs').style.display = 'inline';
+    document.getElementById('acts').style.display = 'inline';
+    document.getElementById('hobbies').style.display = 'inline';
+    document.getElementById('projects').style.display = 'inline';
+}
